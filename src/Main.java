da4c153dcc136dc3ebd5138203e0f6e46ff0ac2f
Junmaku <maxim_kudimov@mail.ru>
2024-12-30 14:39:40 +0900

    Init

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello, World!");
+        //Task 1
+        String firstName = "Ivan";
+        String secondName = "Ivanov";
+        String middleName = "Ivanovich";
+        String fullName = String.format("%s %s %s", secondName, firstName, middleName);
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+        //Task 2
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullName.toUpperCase());
+        //Task 3
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replaceAll("ё", "е");
+        System.out.println("Данные ФИО сотрудника - " + fullName);
     }
 }
